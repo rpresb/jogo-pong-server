23d9138295f2b96fa159be3b755224f02b06f64e
Rodrigo De Presbiteris <rdepresbiteris@twilio.com>
2020-02-20 10:49:09 +0000

    Initial

---
+++ server.js
@@ -0,0 +1,110 @@
+import express from 'express';
+import http from 'http';
+import socketio from 'socket.io';
+
+const app = express();
+const server = http.createServer(app);
+const sockets = socketio(server);
+
+const game = {
+    players: {},
+    rooms: {}
+};
+
+sockets.on('connection', (socket) => {
+    console.log(`${socket.id} conectado.`);
+
+    const name = 'Player_' + socket.id.substr(0, 5);
+    game.players[socket.id] = { name };
+    sendMessage(game.players[socket.id], 'entrou');
+    refreshPlayers();
+    refreshRooms();
+
+    socket.on('disconnect', () => {
+        console.log(`${socket.id} desconectou.`)
+        sendMessage(game.players[socket.id], 'saiu');
+        leaveRoom(socket.id);
+
+        delete game.players[socket.id];
+
+        refreshPlayers();
+        refreshRooms();
+    });
+
+    socket.on('SendMessage', (message) => {
+        sendMessage(game.players[socket.id], message);
+    });
+
+    socket.on('CreateRoom', () => {
+        socket.join(socket.id);
+
+        game.rooms[socket.id] = {
+            name: `Sala do ${game.players[socket.id].name}`,
+            player1: socket.id,
+            player2: undefined
+        };
+
+        game.players[socket.id].room = socket.id;
+
+        refreshPlayers();
+        refreshRooms();
+        sendMessage(game.players[socket.id], 'criou uma sala');
+    });
+
+    socket.on('LeaveRoom', () => {
+        leaveRoom(socket.id);
+
+        refreshPlayers();
+        refreshRooms();
+    });
+
+    socket.on('JoinRoom', (roomId) => {
+        socket.join(roomId);
+
+        const position = game.rooms[roomId].player1 ? '2' : '1';
+
+        game.rooms[roomId][`player${position}`] = socket.id;
+
+        game.players[socket.id].room = roomId;
+
+        refreshPlayers();
+        refreshRooms();
+        sendMessage(game.players[socket.id], 'entrou numa sala');
+    });
+});
+
+const leaveRoom = (socketId) => {
+    const roomId = game.players[socketId].room;
+    const room = game.rooms[roomId];
+
+    if (room) {
+        game.players[socketId].room = undefined;
+
+        if (socketId === room.player1) {
+            room.player1 = undefined;
+        } else {
+            room.player2 = undefined;
+        }
+
+        if (!room.player1 && !room.player2) {
+            delete game.rooms[socketId];
+        }
+    }
+};
+
+const sendMessage = (player, message) => {
+    sockets.emit('ReceiveMessage', `${player.name}: ${message}`);
+};
+
+const refreshPlayers = () => {
+    sockets.emit('PlayersRefresh', game.players);
+};
+
+const refreshRooms = () => {
+    sockets.emit('RoomsRefresh', game.rooms);
+};
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+const port = 4000;
+server.listen(port, () => console.log(`Server rodando na porta ${port}!`));
