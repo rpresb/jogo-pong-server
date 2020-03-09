dc9c336ade609d7848fc3edabca71ecf0392ea00
Rodrigo De Presbiteris <rdepresbiteris@twilio.com>
2020-03-09 16:58:14 +0000

    Add path dependency

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import http from 'http';
 import socketio from 'socket.io';
+import path from 'path';
 
 const app = express();
 const server = http.createServer(app);
